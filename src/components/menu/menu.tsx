ebb70eb0ba3a58a18a96a9f907a1799b55fc647b
LuWroblewski <wrolucas@gmail.com>
2024-09-11 16:46:11 -0300

    fix responsive menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -3,7 +3,7 @@ import { FiHome, FiBarChart2 } from 'react-icons/fi';
 
 export default function Menu() {
   return (
-    <ul className='menu bg-base-200 lg:menu-horizontal rounded-box'>
+    <ul className='menu bg-base-200 menu-horizontal rounded-box'>
       <li>
         <Link href='/'>
           <FiHome className='text-xl' />
