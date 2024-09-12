198c4a6f4397e68cbbdfbbb35d16bd2778b7a7da
LuWroblewski <wrolucas@gmail.com>
2024-09-12 13:20:30 -0300

    change next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['knex'],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/temperature/getLatest',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
