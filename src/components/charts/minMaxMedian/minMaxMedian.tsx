8840f70bbc0d81033aff9b3f32fe555c03fb4520
LuWroblewski <wrolucas@gmail.com>
2024-10-22 21:54:17 -0300

    add verify error

--- src/components/charts/minMaxMedian/minMaxMedian.tsx
+++ src/components/charts/minMaxMedian/minMaxMedian.tsx
@@ -16,55 +16,74 @@ type DataResponse = {
 export default async function MinMaxMedian() {
   const url_api = process.env.URL_API;
 
-  const response = await fetch(`${url_api}/api/charts/minMaxMedian?startDate=2024-09-20&endDate=2024-09-30`, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-    },
-    cache: 'no-cache',
-  });
+  try {
+    const response = await fetch(`${url_api}/api/charts/minMaxMedian?startDate=2024-09-20&endDate=2024-09-30`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+      },
+      cache: 'no-cache',
+    });
 
-  const dataResponse: DataResponse = await response.json();
-  const { temperature, humidity } = dataResponse.data;
+    if (!response.ok) {
+      throw new Error(`Erro ao carregar a API: ${response.status} ${response.statusText}`);
+    }
 
-  return (
-    <div className='flex w-full flex-col lg:flex-row px-16'>
-      <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Media temperatura</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.median} ° </p>
+    const dataResponse: DataResponse = await response.json();
+
+    if (!dataResponse.data) {
+      throw new Error('Dados não disponíveis');
+    }
+
+    const { temperature, humidity } = dataResponse.data;
+
+    return (
+      <div className='flex w-full flex-col lg:flex-row px-16'>
+        <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Media temperatura</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.median} ° </p>
+          </div>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Media umidade</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.median} % </p>
+          </div>
         </div>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Media umidade</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.median} % </p>
+
+        <div className='divider lg:divider-horizontal'></div>
+
+        <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Minimo temperatura</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.min} ° </p>
+          </div>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Minimo umidade</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.min} % </p>
+          </div>
+        </div>
+
+        <div className='divider lg:divider-horizontal'></div>
+
+        <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Maximo temperatura</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.max} ° </p>
+          </div>
+          <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
+            <p className='flex text-3xl'>Maximo umidade</p>
+            <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.max} % </p>
+          </div>
         </div>
       </div>
-
-      <div className='divider lg:divider-horizontal'></div>
-
-      <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Minimo temperatura</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.min} ° </p>
-        </div>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Minimo umidade</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.min} % </p>
+    );
+  } catch (error) {
+    return (
+      <div className='flex w-full flex-col items-center px-16'>
+        <div className='card bg-red-500 text-white rounded-box p-4'>
+          <p className='text-xl'>{(error as Error).message}</p>
         </div>
       </div>
-
-      <div className='divider lg:divider-horizontal'></div>
-
-      <div className='card bg-base-300 rounded-box grid h-auto flex-grow place-items-center'>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Maximo temperatura</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {temperature.max} ° </p>
-        </div>
-        <div className=' justify-center items-center space-x-2 hover:bg-neutral p-2 rounded-box'>
-          <p className='flex text-3xl'>Maximo umidade</p>
-          <p className='justify-center items-center text-center mt-4 text-xl'> {humidity.max} % </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
